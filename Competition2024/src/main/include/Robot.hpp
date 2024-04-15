9393dc46df213501b5a3726e44bffb19966fbe85
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2024-04-15 11:20:12 -0400

    Slimmed down template

--- Competition2024/src/main/include/Robot.hpp
+++ Competition2024/src/main/include/Robot.hpp
@@ -37,15 +37,13 @@ class Robot : public frc::TimedRobot
     private:
         // Robot subassemblies
         Swerve             SWERVE{CHASSIS_LENGTH, CHASSIS_WIDTH};
-        Intake             INTAKE{};
-        Climb              CLIMB{};
 
         // Robot controllers (operator and controller)
-        OperatorController O_CONTROLLER{&INTAKE, &CLIMB};
+        OperatorController O_CONTROLLER{};
         DriverController   D_CONTROLLER{&SWERVE};
 
         // Method to run autonomous commands
-        AutoCommands       Autonomous{&SWERVE, &INTAKE};
+        AutoCommands       Autonomous{&SWERVE};
 
         const std::string kAuto_Do_Nothing     = "Do Nothing";
         const std::string kAuto_Move           = "Auto Move";
@@ -56,41 +54,4 @@ class Robot : public frc::TimedRobot
         frc::SendableChooser<std::string> m_chooser;
         std::string                       m_AutoCommandselected;
 
-        static void VisionThread() 
-        {
-            // Get the USB camera from CameraServer
-            cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
-
-            // Set the resolution
-            camera.SetResolution(640, 480);
-
-            // Get a CvSink. This will capture Mats from the Camera
-            cs::CvSink cvSink = frc::CameraServer::GetVideo();
-
-            // Setup a CvSource. This will send images back to the Dashboard
-            cs::CvSource outputStream = frc::CameraServer::PutVideo("Rectangle", 640, 480);
-
-            // Mats are very memory expensive. Lets reuse this Mat.
-            cv::Mat mat;
-
-            while (true) 
-            {
-                // Tell the CvSink to grab a frame from the camera and put it in the
-                // source mat. If there is an error notify the output.
-                if (cvSink.GrabFrame(mat) == 0) 
-                {
-                    // Send the output the error.
-                    outputStream.NotifyError(cvSink.GetError());
-
-                    // skip the rest of the current iteration
-                    continue;
-                }
-
-                // Put a rectangle on the image
-                rectangle(mat, cv::Point(100, 100), cv::Point(400, 400), cv::Scalar(255, 255, 255), 5);
-
-                // Give the output stream a new image to display
-                outputStream.PutFrame(mat);
-            }
-        }
 };
