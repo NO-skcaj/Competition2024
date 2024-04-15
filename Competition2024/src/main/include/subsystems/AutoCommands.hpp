8de6fd3c0526b9495bd629e4f4919a017385a4e0
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2024-04-15 10:50:40 -0400

    Slimmed down template

--- Competition2024/src/main/include/subsystems/AutoCommands.hpp
+++ Competition2024/src/main/include/subsystems/AutoCommands.hpp
@@ -2,9 +2,6 @@
 
 #include <unistd.h>
 
-#include "Swerve.hpp"
-#include "Intake.hpp"
-
 /// @brief Class to support autonomous commands.
 class AutoCommands
 {
@@ -20,14 +17,7 @@ class AutoCommands
         /// @brief Autonomous command to drive forward.
         void Auto_Move();
 
-        /// @brief  Autonomous comand to place a note in the Amp, and move out of the tape.
-        /// @param move_after_amp - Bool to indacate if it should move after AMP.
-        void Auto_Place_Amp_And_Move(bool move_after_amp);
-
     private:
         /// @brief Pointer to the robot swerve subsystem.
         Swerve *m_swerve;
-
-        /// @brief Pointer to the robot intake subsystem.
-        Intake *m_intake;
 };
