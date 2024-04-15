3b3124c2819bcca24b91ec891d611c80c2279c55
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2024-04-15 12:41:26 -0400

    Added documentation within Controller, same as last commit

--- Competition2024/src/main/cpp/io/DriverController.cpp
+++ Competition2024/src/main/cpp/io/DriverController.cpp
@@ -13,6 +13,27 @@ DriverController::DriverController(Swerve *swerve)
 // @brief Method called periodically every driver/operator control packet.
 void DriverController::Robot_Periodic()
 {
+    // Examples:
+    // d-pad: returns boolean
+    // m_operator_joystick.getPOV() == JOYSTICK_POV_0
+    //                                 JOYSTICK_POV_90
+    //                                 JOYSTICK_POV_180
+    //                                 JOYSTICK_POV_270
+
+    // A B Y X buttons: returns boolean
+    // m_operator_joystick.GetRawButtonPressed(JOYSTICK_BUTTON_A)
+    //                                         JOYSTICK_BUTTON_B
+    //                                         JOYSTICK_BUTTON_X
+    //                                         JOYSTICK_BUTTON_Y
+
+    // Variables for joysticks have not been created, but are used for the example.
+    // Joysticks: returns 1,-1
+    // m_driver_joystick.GetRawAxis(LEFT_JOYSTICK_X)
+    //                              LEFT_JOYSTICK_Y
+    //                              RIGHT_JOYSTICK_X
+    //                              RIGHT_JOYSTICK_Y
+    // I'm also fairly certain that m_driver_joystick.GetY() also works
+ 
     // Determine if the reset gyro joystick button was pressed
     if (m_driver_joystick.GetRawButtonPressed(1))
         m_swerve->navx.Reset();
