0fb7789f10579fa097b3742190cfdeea4e70a2f5
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2024-04-15 11:18:58 -0400

    Slimmed down template

--- Competition2024/src/main/include/Constants.hpp
+++ Competition2024/src/main/include/Constants.hpp
@@ -21,12 +21,6 @@
 #define RL_E_CAN_ID                             40
 #define RR_E_CAN_ID                             42
 
-#define INTAKE_ACTUATION_CAN_ID                 20
-#define INTAKE_ROLLER_MOTOR_CAN_ID              21
-
-#define CLIMBER_RIGHT_CAN_ID                    30
-#define CLIMBER_LEFT_CAN_ID                     31
-
 // 180 / Pi
 #define MAGIC_NUMBER                      57.29577f
 #define ANTIMAGIC_NUMBER                   0.01745f
@@ -76,45 +70,6 @@
 #define ANGLES_SWERVE_P                       0.05
 #define ANGLES_SWERVE_I                       0.00
 #define ANGLES_SWERVE_D                       0.00
-
-// ### Intake variables ###
-#define INTAKE_ROLLER_MOTOR_JOYSTICK_DEADBAND  0.1
-#define INTAKE_MAXIMUM_ROLLER_POWER            0.5  // MAximum power to the roller motor
-
-#define INTAKE_ACTUATION_SENSOR_RATIO          1.0  // Adjust per robot (could set to 1 and just use encoder counts)
-
-#define INTAKE_ACTUATION_A                   0.000  // Adceleration FeedForward
-#define INTAKE_ACTUATION_D                   0.000  // Derivative Gain
-#define INTAKE_ACTUATION_G                   0.000  // Gravity FeedForward Gain
-#define INTAKE_ACTUATION_I                   1.000  // Integra Gain
-#define INTAKE_ACTUATION_P                  25.000  // Proportional Gain
-#define INTAKE_ACTUATION_S                   0.000  // Static FeedForward Gain
-#define INTAKE_ACTUATION_V                   1.000  // Velocity FeedFroward Gain
-
-#define INTAKE_ACTUATION_ACCELERATION      150.000  // Move Acceleration       - INCREASE FOR FASTER ACCELERATION
-#define INTAKE_ACTUATION_VELOCITY          100.000  // Maximum Move Velocity   - SETS MAXIMUM SPEED
-#define INTAKE_ACTUATION_JERK              200.000  // Acceleration Derivative - INCREASE FOR MORE SPEED (200 - 500)
-
-#define INTAKE_START_POSITION                0.000  // Should always start at zero
-#define INTAKE_AMP_POSITION                  5.700  // Adjust to the desired Amp position
-#define INTAKE_FEED_POSITION                 8.500  // Adjust to the desired Feed position
-#define INTAKE_CLIMB_POSITION               12.000 // Adust to be out of the way for climb.
-#define INTAKE_POSITION_REACHED_POSITION     0.500  // Indicates the position offset to change intake position (Amp or Feed)
-
-#define INTAKE_POSITION_STEP_VALUE           0.100  // Sets the "jog" step size
-
-#define INTAKE_MAX_AMPERAGE                     30  // The maximum intake motor amperage
-
-// ### Climber Variable ###
-#define CLIMBER_MOTORS                           2
-
-#define CLIMBER_MOTOR_RIGHT                      0
-#define CLIMBER_MOTOR_LEFT                       1
-
-#define CLIMB_MAXIMUM_OUTPUT_CURRENT            60  // Threshold to disable the climb motors (reached full climb)
-
-#define CLIMBER_UP_POWER                       1.0
-#define CLIMBER_DOWN_POWER                    -1.0
 
 // ### Autonomous Variables ###
 // Autonomous Move
